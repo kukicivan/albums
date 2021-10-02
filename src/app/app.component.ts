4cc26e981dca7eadc4ce72c3081dc25b68ea435a
Ivan Kukic <ivan.kukic@gmail.com>
2021-10-02 08:12:23 +0200

    add labels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -7,9 +7,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  labels = ['Family', 'Friends', 'Travel'];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
